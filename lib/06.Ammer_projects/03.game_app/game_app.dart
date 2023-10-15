cab572f9ed58f3a35e3beb3626b401b1db46b7a5
AyA_Abd_Elmoneim2 <ayaabdelmon@gmail.com>
2023-10-15 17:20:35 +0000

    🚩06. Ammar_projects_2023 👉 Add gameApp (Add Logic)

--- lib/06.Ammer_projects/03.game_app/game_app.dart
+++ lib/06.Ammer_projects/03.game_app/game_app.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_projects/06.Ammer_projects/03.game_app/widgets/game_app_bar.dart';
 import 'package:flutter_projects/06.Ammer_projects/03.game_app/widgets/game_body.dart';
 
-class GameApp extends StatefulWidget {
+class GameApp extends StatelessWidget {
   const GameApp({Key? key}) : super(key: key);
 
-  @override
-  State<GameApp> createState() => _GameAppState();
-}
-
-class _GameAppState extends State<GameApp> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
--- lib/06.Ammer_projects/03.game_app/widgets/game_body.dart
+++ lib/06.Ammer_projects/03.game_app/widgets/game_body.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class GameBody extends StatelessWidget {
+class GameBody extends StatefulWidget {
   const GameBody({
     super.key,
   });
+
+  @override
+  State<GameBody> createState() => _GameBodyState();
+}
+
+class _GameBodyState extends State<GameBody> {
+  int leftImageNum = 1;
+  int rightImageNum = 1;
+
+  void changeImageNum() {
+    leftImageNum = Random().nextInt(8) + 1;
+    rightImageNum = Random().nextInt(8) + 1;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,17 +26,38 @@ class GameBody extends StatelessWidget {
       child: Column(
         children: [
           Text(
-            "Try Again...!",
+            (leftImageNum != rightImageNum)
+                ? "Try Again...!"
+                : "Congration...!",
             style: Theme.of(context).textTheme.bodyMedium,
           ),
           const SizedBox(height: 24),
           Row(
             children: [
-              Expanded(child: Image.asset("assets/gameImages/image-1.png")),
-              Expanded(child: Image.asset("assets/gameImages/image-2.png")),
+              ImageItem(imageNum: leftImageNum),
+              ImageItem(imageNum: rightImageNum),
             ],
           )
         ],
+      ),
+    );
+  }
+}
+
+class ImageItem extends StatelessWidget {
+  const ImageItem({
+    super.key,
+    required this.imageNum,
+  });
+
+  final int imageNum;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        onTap: () => changeImageNum(),
+        child: Image.asset('assets/gameImages/image-$imageNum.png'),
       ),
     );
   }
