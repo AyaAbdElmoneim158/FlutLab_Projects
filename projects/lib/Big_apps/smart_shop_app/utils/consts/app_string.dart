c64f6c22323619aad26759b397c068b76d83023d
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2023-11-14 04:16:28 +0200

    delete Projects Dir

--- projects/lib/Big_apps/smart_shop_app/utils/consts/app_string.dart
+++
@@ -1,5 +0,0 @@
-class AppString {
-  static const String appTitle = 'Smart Shop';
-  static const String themeKey = 'themeStatus';
-
-}
--- projects/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:projects/Big_apps/smart_shop_app/smart_shop_app.dart';
-import 'package:projects/Big_apps/smart_shop_app/utils/helper/shared_preferences.dart';
-
-void main() {
-  SharedPref.init();
-  runApp(const SmartShopApp());
-}
